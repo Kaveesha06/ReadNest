fb45bdfb2048629bc28d6aff63b4653775cc9ab9
Kaveesha Archana <kaveeshaarchana2003@gmail.com>
2025-07-18 09:28:54 +0530

    Initial commit

---
+++ src/java/hibernate/HibernateUtil.java
@@ -0,0 +1,37 @@
+package hibernate;
+
+import java.io.InputStream;
+import java.util.Properties;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+
+    static {
+
+        try {
+
+            Configuration configure = new Configuration().configure();
+            
+            sessionFactory = configure.buildSessionFactory();
+
+        } catch (Throwable ex) {
+
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+
+        }
+
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+
+}
