6d9fb58d819411634c224675ae78184eee64aed7
Kaveesha Archana <kaveeshaarchana2003@gmail.com>
2025-07-19 12:22:59 +0530

    Add user registration backend and frontend assets
    Implemented user registration functionality with a new SignUp servlet, Hibernate User entity, and supporting utility and mail classes. Updated Hibernate configuration to map the User entity. Added session filter for authentication-related pages. Introduced frontend assets including CSS, JS, images, and HTML pages for sign-up, sign-in, and account verification. Added .gitignore for build directory.

---
+++ web/assets/js/sign-up.js
@@ -0,0 +1,56 @@
+async function signUp(){
+//    console.log("OK");
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    
+//    console.log(firstName);
+//    console.log(lastName);
+//    console.log(email);
+//    console.log(password);
+
+    const user = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password
+    };
+    
+    const userJson = JSON.stringify(user);
+    
+    const response = await fetch(
+            "SignUp",
+    {
+        method:"POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: userJson
+    }
+    );
+    
+    if(response.ok) {
+        const json = await response.json();
+//        console.log(json);
+
+        if(json.status){ //if true
+//            //redirect another page
+            document.getElementById("message").className = "text-success";
+            document.getElementById("message").innerHTML = json.message;
+            windows.location = "verify-account.html";
+//            
+        }else{  // status false
+//            //custom message
+//          console.log(json.message);            
+            document.getElementById("message").innerHTML = json.message;
+
+            
+        }
+    }else{
+//        console.log(json.message);
+        document.getElementById("message").innerHTML = "Registration failed. Please try again.! ";
+    }
+
+}
+
